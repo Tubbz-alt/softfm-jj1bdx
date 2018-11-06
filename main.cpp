9321725c80532d790a05a1d0b9a1fc2d553acb41
Kenji Rikitake <kenji.rikitake@acm.org>
2018-11-06 09:01:13 +0900

    main.cpp: put class DataBuffer to DataBuffer.h

--- main.cpp
+++ main.cpp
@@ -33,6 +33,7 @@
 #include <unistd.h>
 
 #include "AudioOutput.h"
+#include "DataBuffer.h"
 #include "FmDecode.h"
 #include "RtlSdrSource.h"
 #include "SoftFM.h"
@@ -40,75 +41,6 @@
 
 // Flag is set on SIGINT / SIGTERM.
 static std::atomic_bool stop_flag(false);
-
-// Buffer to move sample data between threads.
-template <class Element> class DataBuffer {
-public:
-  // Constructor.
-  DataBuffer() : m_qlen(0), m_end_marked(false) {}
-
-  // Add samples to the queue.
-  void push(std::vector<Element> &&samples) {
-    if (!samples.empty()) {
-      std::unique_lock<std::mutex> lock(m_mutex);
-      m_qlen += samples.size();
-      m_queue.push(move(samples));
-      lock.unlock();
-      m_cond.notify_all();
-    }
-  }
-
-  // Mark the end of the data stream.
-  void push_end() {
-    std::unique_lock<std::mutex> lock(m_mutex);
-    m_end_marked = true;
-    lock.unlock();
-    m_cond.notify_all();
-  }
-
-  // Return number of samples in queue.
-  size_t queued_samples() {
-    std::unique_lock<std::mutex> lock(m_mutex);
-    return m_qlen;
-  }
-
-  // If the queue is non-empty, remove a block from the queue and
-  // return the samples. If the end marker has been reached, return
-  // an empty vector. If the queue is empty, wait until more data is pushed
-  // or until the end marker is pushed.
-  std::vector<Element> pull() {
-    std::vector<Element> ret;
-    std::unique_lock<std::mutex> lock(m_mutex);
-    while (m_queue.empty() && !m_end_marked)
-      m_cond.wait(lock);
-    if (!m_queue.empty()) {
-      m_qlen -= m_queue.front().size();
-      swap(ret, m_queue.front());
-      m_queue.pop();
-    }
-    return ret;
-  }
-
-  // Return true if the end has been reached at the Pull side.
-  bool pull_end_reached() {
-    std::unique_lock<std::mutex> lock(m_mutex);
-    return m_qlen == 0 && m_end_marked;
-  }
-
-  // Wait until the buffer contains minfill samples or an end marker.
-  void wait_buffer_fill(size_t minfill) {
-    std::unique_lock<std::mutex> lock(m_mutex);
-    while (m_qlen < minfill && !m_end_marked)
-      m_cond.wait(lock);
-  }
-
-private:
-  std::size_t m_qlen;
-  bool m_end_marked;
-  std::queue<std::vector<Element>> m_queue;
-  std::mutex m_mutex;
-  std::condition_variable m_cond;
-};
 
 // Simple linear gain adjustment.
 void adjust_gain(SampleVector &samples, double gain) {
