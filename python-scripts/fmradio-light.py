ba79ee76d7f2ce0f1ede6e075fd58ff0c2a2a24b
Kenji Rikitake <kenji.rikitake@acm.org>
2018-11-07 17:11:12 +0900

    Add python scripts for invoking softfm commands

---
+++ python-scripts/fmradio-light.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Simple caller for softfm
+import sys
+import subprocess
+import signal
+
+argvs = sys.argv
+argc = len(argvs)
+
+def signal_handler(signal, frame):
+    print('Terminated by CTRL/C')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if argc != 2:
+    print('Usage: ', argvs[0], '<frequency in MHz>\n')
+    quit()
+freq = float(argvs[1]) * 1000000
+command = "softfm -f " + str(freq) + " -g 8.7 -s 240000 -r44100 -b 2.0 -q -R - | " + \
+          "play -t raw -esigned-integer -b16 -r 44100 -c 2 -q -"
+subprocess.run(command, shell=True, check=True)
