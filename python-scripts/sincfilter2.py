0c7ccce24f30e7804841cd0409bd9d48d2fea931
Kenji Rikitake <kenji.rikitake@acm.org>
2019-01-06 17:32:43 +0900

    Add sincfilter2.py

---
+++ python-scripts/sincfilter2.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+# Curve-fitting calculation script for sinc compensation
+
+import math
+
+def aperture(x):
+    if x == 0.0:
+        return 1.0
+    else:
+        return 2.0 / x * math.sin(x/2.0)
+
+def mov1(x):
+    y = math.sin(x/4.0);
+    if y == 0.0:
+        return 0.0
+    else:
+        return 0.5 * math.sin(x/2.0) / math.sin (x/4.0)
+
+maxfreq = 120000
+fitfactor = 0.520374809
+sqsum_logratio = 0
+for freq in range(50,54000,1000):
+    theta = 2 * math.pi * freq / maxfreq;
+    compensate = 1.0 / aperture(theta)
+    # filter model: 
+    # mov1: moving-average filter (stage number: 1)
+    # mov1[n] = (input[n-1] + input[n]) / 2.0
+    # final output: gain-controlled sum of the above two filters
+    # output[n] = 4.0 * input[n] - fitfactor * mov1[n]
+    fitlevel = 1.5 - (fitfactor * mov1(2 * theta))
+    logratio = math.log10(compensate / fitlevel)
+    sqsum_logratio += logratio * logratio
+    output = "freq = " + str(freq);
+    output += " compensate = " + str(compensate)
+    output += " fitlevel = " + str(fitlevel)
+    output += " logratio = " + str(logratio)
+    print(output)
+print("factor = " + str(fitfactor) + " sqsum_logratio = " + str(sqsum_logratio))
+
